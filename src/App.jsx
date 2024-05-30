df50d7e03f0293a7af45d401d925df00a9cd2a83
nachobaez96 <nachobaez96@gmail.com>
2024-05-30 17:30:10 +0200

    added switch drumkit feature, cleaned up css

--- src/App.jsx
+++ src/App.jsx
@@ -12,20 +12,44 @@ const kit1 = [
   { name: "Kick-n'-Hat", link: "./assets/audio/Kick_n_Hat.mp3" },
   { name: "Kick", link: "./assets/audio/Kick.mp3" },
   { name: "Closed-HH", link: "./assets/audio/Closed-HH.mp3" }
-];
+]
+
+const kit2 = [
+  { name: "Alert", link: "./assets/audio/kit2/Alert.mp3" },
+  { name: "Boom", link: "./assets/audio/kit2/Boom.mp3" },
+  { name: "Dolphin", link: "./assets/audio/kit2/Dolphin.mp3" },
+  { name: "Glass", link: "./assets/audio/kit2/Glass.mp3" },
+  { name: "Reload", link: "./assets/audio/kit2/Reload.mp3" },
+  { name: "Rubber-Duck", link: "./assets/audio/kit2/Rubber-Duck.mp3" },
+  { name: "Slap", link: "./assets/audio/kit2/Slap.mp3" },
+  { name: "Wilhelm", link: "./assets/audio/kit2/Wilhelm.mp3" },
+  { name: "Yeet", link: "./assets/audio/kit2/Yeet.mp3" }
+]
 
 
 function App() {
-
-  const [sounds, setSounds] = React.useState(kit1)
+  const [sounds, setSounds] = useState(kit1)
   const [display, setDisplay] = useState('')
+  const [isKit1, setIsKit1] = useState(true)
 
   const updateDisplay = (name) => {
     setDisplay(name)
   }
 
+  const switchKit = () => {
+    if (isKit1) {
+      setSounds(kit2)
+    } else {
+      setSounds(kit1)
+    }
+    setIsKit1(!isKit1)
+  }
+
   return (
     <div id="drum-machine">
+      <div className="controls">
+        <button onClick={switchKit}>Switch Kit</button>
+      </div>
       <div className="squares-container">
         <Pad name={sounds[0].name} link={sounds[0].link} triggerKey={'q'} updateDisplay={updateDisplay} />
         <Pad name={sounds[1].name} link={sounds[1].link} triggerKey={'w'} updateDisplay={updateDisplay} />
