c258143b2f1df7f7a1e9a64deb154b6d1853afd5
nachobaez96 <nachobaez96@gmail.com>
2024-05-30 02:38:14 +0200

    added display div, cleaned up css

--- src/App.jsx
+++ src/App.jsx
@@ -18,21 +18,26 @@ const kit1 = [
 function App() {
 
   const [sounds, setSounds] = React.useState(kit1)
+  const [display, setDisplay] = useState('')
+
+  const updateDisplay = (name) => {
+    setDisplay(name)
+  }
 
   return (
     <div id="drum-machine">
       <div className="squares-container">
-        <Pad name={sounds[0].name} link={sounds[0].link} triggerKey={'q'} />
-        <Pad name={sounds[1].name} link={sounds[1].link} triggerKey={'w'} />
-        <Pad name={sounds[2].name} link={sounds[2].link} triggerKey={'e'} />
-        <Pad name={sounds[3].name} link={sounds[3].link} triggerKey={'a'} />
-        <Pad name={sounds[4].name} link={sounds[4].link} triggerKey={'s'} />
-        <Pad name={sounds[5].name} link={sounds[5].link} triggerKey={'d'} />
-        <Pad name={sounds[6].name} link={sounds[6].link} triggerKey={'z'} />
-        <Pad name={sounds[7].name} link={sounds[7].link} triggerKey={'x'} />
-        <Pad name={sounds[8].name} link={sounds[8].link} triggerKey={'c'} />
+        <Pad name={sounds[0].name} link={sounds[0].link} triggerKey={'q'} updateDisplay={updateDisplay} />
+        <Pad name={sounds[1].name} link={sounds[1].link} triggerKey={'w'} updateDisplay={updateDisplay} />
+        <Pad name={sounds[2].name} link={sounds[2].link} triggerKey={'e'} updateDisplay={updateDisplay} />
+        <Pad name={sounds[3].name} link={sounds[3].link} triggerKey={'a'} updateDisplay={updateDisplay} />
+        <Pad name={sounds[4].name} link={sounds[4].link} triggerKey={'s'} updateDisplay={updateDisplay} />
+        <Pad name={sounds[5].name} link={sounds[5].link} triggerKey={'d'} updateDisplay={updateDisplay} />
+        <Pad name={sounds[6].name} link={sounds[6].link} triggerKey={'z'} updateDisplay={updateDisplay} />
+        <Pad name={sounds[7].name} link={sounds[7].link} triggerKey={'x'} updateDisplay={updateDisplay} />
+        <Pad name={sounds[8].name} link={sounds[8].link} triggerKey={'c'} updateDisplay={updateDisplay} />
       </div>
-      <div className="display">Name of button here</div>
+      <div className="display">{display}</div>
     </div>
   )
 }
--- src/Pad.jsx
+++ src/Pad.jsx
@@ -9,21 +9,22 @@ export default function Pad(props) {
     const playSound = () => {
         const audio = new Audio(props.link)
         audio.play()
+        props.updateDisplay(props.name)
     }
 
     useEffect(() => {
         const handleKeyDown = (event) => {
             if (event.key === props.triggerKey) {
-                playSound();
+                playSound()
             }
         };
 
-        document.body.addEventListener('keydown', handleKeyDown);
+        document.body.addEventListener('keydown', handleKeyDown)
 
         return () => {
-            document.body.removeEventListener('keydown', handleKeyDown);
-        };
-    }, [props.triggerKey, props.link]);
+            document.body.removeEventListener('keydown', handleKeyDown)
+        }
+    }, [props.triggerKey, props.link])
 
     return (
         <div className="pad" onClick={playSound} tabIndex="0">
