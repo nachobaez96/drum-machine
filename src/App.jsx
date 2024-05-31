c7b44c12d36332a664468a414176e208df2522dc
nachobaez96 <nachobaez96@gmail.com>
2024-05-31 23:16:33 +0200

    features: upload audio + splice

--- src/App.jsx
+++ src/App.jsx
@@ -3,47 +3,66 @@ import './App.css'
 import Pad from './Pad.jsx'
 
 const kit1 = [
-  { name: "Heater 1", link: "./assets/audio/Heater-1.mp3" },
-  { name: "Heater 2", link: "./assets/audio/Heater-2.mp3" },
-  { name: "Heater 3", link: "./assets/audio/Heater-3.mp3" },
-  { name: "Heater 4", link: "./assets/audio/Heater-4.mp3" },
-  { name: "Clap", link: "./assets/audio/Clap.mp3" },
-  { name: "Open-HH", link: "./assets/audio/Open-HH.mp3" },
-  { name: "Kick-n'-Hat", link: "./assets/audio/Kick_n_Hat.mp3" },
-  { name: "Kick", link: "./assets/audio/Kick.mp3" },
-  { name: "Closed-HH", link: "./assets/audio/Closed-HH.mp3" }
+  { name: "Heater 1", link: "./assets/audio/Heater-1.mp3", startTime: 0, triggerKey: 'q' },
+  { name: "Heater 2", link: "./assets/audio/Heater-2.mp3", startTime: 0, triggerKey: 'w' },
+  { name: "Heater 3", link: "./assets/audio/Heater-3.mp3", startTime: 0, triggerKey: 'e' },
+  { name: "Heater 4", link: "./assets/audio/Heater-4.mp3", startTime: 0, triggerKey: 'a' },
+  { name: "Clap", link: "./assets/audio/Clap.mp3", startTime: 0, triggerKey: 's' },
+  { name: "Open-HH", link: "./assets/audio/Open-HH.mp3", startTime: 0, triggerKey: 'd' },
+  { name: "Kick-n'-Hat", link: "./assets/audio/Kick_n_Hat.mp3", startTime: 0, triggerKey: 'z' },
+  { name: "Kick", link: "./assets/audio/Kick.mp3", startTime: 0, triggerKey: 'x' },
+  { name: "Closed-HH", link: "./assets/audio/Closed-HH.mp3", startTime: 0, triggerKey: 'c' }
 ]
 
 const kit2 = [
-  { name: "Alert", link: "./assets/audio/kit2/Alert.mp3" },
-  { name: "Boom", link: "./assets/audio/kit2/Boom.mp3" },
-  { name: "Dolphin", link: "./assets/audio/kit2/Dolphin.mp3" },
-  { name: "Glass", link: "./assets/audio/kit2/Glass.mp3" },
-  { name: "Reload", link: "./assets/audio/kit2/Reload.mp3" },
-  { name: "Rubber-Duck", link: "./assets/audio/kit2/Rubber-Duck.mp3" },
-  { name: "Slap", link: "./assets/audio/kit2/Slap.mp3" },
-  { name: "Wilhelm", link: "./assets/audio/kit2/Wilhelm.mp3" },
-  { name: "Yeet", link: "./assets/audio/kit2/Yeet.mp3" }
+  { name: "Alert", link: "./assets/audio/kit2/Alert.mp3", startTime: 0, triggerKey: 'q' },
+  { name: "Boom", link: "./assets/audio/kit2/Boom.mp3", startTime: 0, triggerKey: 'w' },
+  { name: "Dolphin", link: "./assets/audio/kit2/Dolphin.mp3", startTime: 0, triggerKey: 'e' },
+  { name: "Glass", link: "./assets/audio/kit2/Glass.mp3", startTime: 0, triggerKey: 'a' },
+  { name: "Reload", link: "./assets/audio/kit2/Reload.mp3", startTime: 0, triggerKey: 's' },
+  { name: "Rubber-Duck", link: "./assets/audio/kit2/Rubber-Duck.mp3", startTime: 0, triggerKey: 'd' },
+  { name: "Slap", link: "./assets/audio/kit2/Slap.mp3", startTime: 0, triggerKey: 'z' },
+  { name: "Wilhelm", link: "./assets/audio/kit2/Wilhelm.mp3", startTime: 0, triggerKey: 'x' },
+  { name: "Yeet", link: "./assets/audio/kit2/Yeet.mp3", startTime: 0, triggerKey: 'c' }
 ]
 
+const kitUser = Array(9).fill().map((_, index) => ({
+  name: "Upload",
+  link: "",
+  startTime: 0,
+  triggerKey: ['q', 'w', 'e', 'a', 's', 'd', 'z', 'x', 'c'][index]
+}))
+
+const kits = [kit1, kit2, kitUser]
 
 function App() {
-  const [sounds, setSounds] = useState(kit1)
+  const [currentKitIndex, setCurrentKitIndex] = useState(0)
+  const [sounds, setSounds] = useState(kits[currentKitIndex])
   const [display, setDisplay] = useState('')
-  const [isKit1, setIsKit1] = useState(true)
 
   const updateDisplay = (name) => {
     setDisplay(name)
   }
 
   const switchKit = () => {
-    if (isKit1) {
-      setSounds(kit2)
-    } else {
-      setSounds(kit1)
-    }
-    setIsKit1(!isKit1)
+    const nextIndex = (currentKitIndex + 1) % kits.length
+    setCurrentKitIndex(nextIndex)
+    setSounds(kits[nextIndex])
   }
+
+  const updateSound = (index, link, name) => {
+    setSounds((prevSounds) => {
+        const newSounds = [...prevSounds]
+        newSounds[index] = { ...newSounds[index], link, name }
+        return newSounds
+    })
+    
+    setKits((prevKits) => {
+      const newKits = [...prevKits]
+      newKits[currentKitIndex] = [...prevSounds]
+      return newKits
+  })
+}
 
   return (
     <div id="drum-machine">
@@ -51,15 +70,19 @@ function App() {
         <button onClick={switchKit}>Switch Kit</button>
       </div>
       <div className="squares-container">
-        <Pad name={sounds[0].name} link={sounds[0].link} triggerKey={'q'} updateDisplay={updateDisplay} />
-        <Pad name={sounds[1].name} link={sounds[1].link} triggerKey={'w'} updateDisplay={updateDisplay} />
-        <Pad name={sounds[2].name} link={sounds[2].link} triggerKey={'e'} updateDisplay={updateDisplay} />
-        <Pad name={sounds[3].name} link={sounds[3].link} triggerKey={'a'} updateDisplay={updateDisplay} />
-        <Pad name={sounds[4].name} link={sounds[4].link} triggerKey={'s'} updateDisplay={updateDisplay} />
-        <Pad name={sounds[5].name} link={sounds[5].link} triggerKey={'d'} updateDisplay={updateDisplay} />
-        <Pad name={sounds[6].name} link={sounds[6].link} triggerKey={'z'} updateDisplay={updateDisplay} />
-        <Pad name={sounds[7].name} link={sounds[7].link} triggerKey={'x'} updateDisplay={updateDisplay} />
-        <Pad name={sounds[8].name} link={sounds[8].link} triggerKey={'c'} updateDisplay={updateDisplay} />
+        {sounds.map((sound, index) => (
+          <Pad
+            key={index}
+            name={sound.name}
+            link={sound.link}
+            startTime={sound.startTime}
+            triggerKey={sound.triggerKey}
+            updateDisplay={updateDisplay}
+            isCustomKit={currentKitIndex === 2}
+            updateSound={updateSound}
+            index={index}
+          />
+        ))}
       </div>
       <div className="display">{display}</div>
     </div>
